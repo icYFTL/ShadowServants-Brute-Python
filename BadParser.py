3aca749f4293356120cd3619bea0d0e01576b1f4
Pavlov <savap0@yandex.ru>
2019-02-22 04:58:15 +0300

    Bad Parser update [retry]

--- BadParser.py
+++ BadParser.py
@@ -1,53 +1,35 @@
+# Version 0.1
+
 import cfscrape
-from bs4 import BeautifulSoup
 import re
+import Stopwatch
 
 class BadParser(object):
     def Grab():
         r = cfscrape.create_scraper()
-
-
-        print('[Bad parser v0.1 beta]\n')
-        print('Really works bad because I was writing this at 04:00 am. Don\'t hit me, pls. I\'ll re-write it. May be. 50/50 =)\nYou can stop parse by CTRL+C.\n\n\n')
-
-
-        print('Wait for data from proxy server.')
-
-        data = r.get("https://hidemyna.me/ru/proxy-list/").text
-
-        print('Got data. Parsing initializated.\n')
-
-        soup = BeautifulSoup(data, 'html.parser')
-
+        handled = 0
         true_set = []
 
-        parsed = soup.findAll('td')
-        proxy = re.findall(r'[0-9]+.[0-9]+.[0-9]+.[0-9]+',str(parsed))
-        for i in proxy:
-            if (len(i) > 0):
+        print('[Bad parser v0.2 beta]\n')
+
+        print('Wait for data from proxy server.\n')
+        timer = Stopwatch.create_thread()
+        data = r.get("https://hidemyna.me/ru/proxy-list/").text
+        print('Got data. Parsing initializated.\nTime spent: '+str(timer.secs) + ' secs.\n')
+        timer.kill_thread()
+
+        try:
+
+            proxy = re.findall(r'<td class=tdl>[0-9]+.[0-9]+.[0-9]+.[0-9]+<\/td><td>[0-9]{1,5}', data)
+
+            for i in proxy:
+                i = str(i)
+                i = i.replace('<td class=tdl>','')
+                i = i.replace('</td><td>',':')
                 true_set.append(i)
-
-            for i in range(len(parsed)):
-                try:
-                    for j in true_set:
-                        if j in parsed[i]:
-                           parsed[i] = None
-                except TypeError:
-                    continue
-        count = 0
-        for i in range(len(str(parsed))):
-            try:
-                if str.isdecimal(str(parsed)[i]):
-                    if str(parsed)[i-1] == '>' and str(parsed)[i-2] == 'd':
-                        true_set[count] += ":"
-                        while(str.isdecimal(str(parsed)[i])):
-                            true_set[count]+=str(parsed)[i]
-                            i += 1
-                        print("Proxies handled: " + str(count))
-                        count+=1
-            except TypeError:
-                continue
-            except KeyboardInterrupt:
-                print('\n\nOk. Parsing done.\nHandled: ' + str(count-1))
-                break
-
+                handled += 1
+                print('Proxy handled: ' + str(handled))
+            raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            print('\nOk. Work done.')
+            return true_set
